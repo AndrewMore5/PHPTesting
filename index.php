0449d60894c7d4cda96e13377185c1c51b966640
Andrew More <andrew.more@zaltek.co.uk>
2025-09-01 10:50:02 +0100

    Testing Comments

--- index.php
+++ index.php
@@ -7,12 +7,19 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
         <tite>Document</title>
+        
 </head>
 <body>
     <?php
+    // This is a comment. It should nbe in the PHP arae
         echo "Hello PHP <br>";
         echo "This should be on a new line? <br>";
-        echo "Value 1" . 'Thing 2' . "Thing 3!!! <br>";     
+        echo "Value 1" . 'Thing 2' . "Thing 3!!! <br>";
+     /* This is a comment
+     THat spans
+     MULTIPLE LINES
+     If you see this I have done something wrong
+     */        
     ?>
 </body>
 </html>
