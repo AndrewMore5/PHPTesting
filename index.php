268fe610e010e73b77653cca12aef0cebc25d223
Andrew More <andrew.more@zaltek.co.uk>
2025-09-01 11:01:45 +0100

    Testing variables

--- index.php
+++ index.php
@@ -5,21 +5,22 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, inital-scale=1.0">
-        <tite>Document</title>
-        
+        <meta name="viewport" content="width=device-width, inital-scale=1.0">      
 </head>
 <body>
     <?php
     // This is a comment. It should nbe in the PHP arae
-        echo "Hello PHP <br>";
-        echo "This should be on a new line? <br>";
-        echo "Value 1" . 'Thing 2' . "Thing 3!!! <br>";
+    echo "Hello PHP <br>";
+    echo "This should be on a new line? <br>";
+    echo "Value 1" . 'Thing 2' . "Thing 3!!! <br>";
      /* This is a comment
      THat spans
      MULTIPLE LINES
      If you see this I have done something wrong
-     */        
+     */
+    $name = "Richard";
+    $location = "corner";
+    echo "My name is " . $name . " And I'm sitting in the " . $location . "!<br>";
     ?>
 </body>
 </html>
